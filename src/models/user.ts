7c6b9dab225bd445c514f39849fbe38c56734298
Emmanuel Oluwole <s00248432@atu.ie>
2025-11-06 12:20:12 +0000

    updates done

--- src/models/user.ts
+++ src/models/user.ts
@@ -15,7 +15,7 @@ export interface User {
 
 export const createUserSchema = z.object({
   fullName: z.string().min(2).max(100),
-  email: z.string().email(),
+  email: z.email(),
   passwordHash: z.string().min(6),
   address: z.string(),
   phoneNumber: z.string().regex(/^\+353\d{7,12}$/),
